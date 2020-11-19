daf72237ffeaf7ed652f48377d30687f2baa7fc9
drillprop <drylbartosz@gmail.com>
2020-11-19 21:03:51 +0100

    Create CartDropdown and add to Header

---
+++ src/components/Header/CartDropdown/index.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { useCartContext } from "../../../contexts/CartContext"
+import styles from "./cart-dropdown.module.scss"
+
+export default function CartDropdown() {
+  const { cartItems, toggleCart } = useCartContext()
+  const handleCloseCart = (e) => {
+    // little trick to handle outside click
+    if (e.currentTarget === e.target) {
+      toggleCart()
+    }
+  }
+  return (
+    <div className={styles.cartDropdownOverlay} onClick={handleCloseCart}>
+      <div className={styles.cartDropdownBox}>
+        <ul className={styles.cartDropdownList}>
+          {cartItems.map((item, idx) => {
+            console.log(item)
+            return (
+              <li className={styles.cartDropdownItem} key={item.slug + idx}>
+                <img
+                  className={styles.cartDropdownImg}
+                  src={item.image}
+                  alt={item.name}
+                />
+                <p className={styles.cartDropdownName}>{item.name}</p>
+                <p className={styles.cartDropdownPrice}>${item.price}</p>
+              </li>
+            )
+          })}
+        </ul>
+        <button className={styles.cartDropdownButton}>submit</button>
+      </div>
+    </div>
+  )
+}
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,17 +2,29 @@ import React from "react"
 import { Link } from "gatsby"
 import styles from "./header.module.scss"
 import Carticon from "../../assets/icons/cart.svg"
+import { useCartContext } from "../../contexts/CartContext"
+import CartDropdown from "./CartDropdown"
 
 export default function Header() {
+  const { isCartOpen, toggleCart, cartItemsAmount } = useCartContext()
   return (
-    <header className={styles.header}>
-      <h1 className={styles.logo}>
-        <Link to="/">JAM SHOP</Link>
-      </h1>
-      <button className={styles.cartButton} title="open cart">
-        <img className={styles.cartIcon} src={Carticon} alt="cart icon" />
-        <span className={styles.cartQuantity}>0</span>
-      </button>
-    </header>
+    <>
+      <header className={styles.header}>
+        <h1 className={styles.logo}>
+          <Link to="/">JAM SHOP</Link>
+        </h1>
+        <div>
+          <button
+            className={styles.cartButton}
+            title="open cart"
+            onClick={toggleCart}
+          >
+            <img className={styles.cartIcon} src={Carticon} alt="cart icon" />
+            <span className={styles.cartQuantity}>{cartItemsAmount}</span>
+          </button>
+        </div>
+      </header>
+      {isCartOpen && <CartDropdown />}
+    </>
   )
 }
