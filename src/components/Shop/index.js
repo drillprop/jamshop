fd0212e8a32107be3354d4b8e6295b0cc7cc5b94
drillprop <drylbartosz@gmail.com>
2020-11-20 00:28:08 +0100

    Create Shop component

---
+++ src/components/Shop/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import styles from "./shop.module.scss"
+import ShopCarousel from "./ShopCarousel"
+import ShopHeader from "./ShopHeader"
+
+export default function Shop() {
+  return (
+    <section className={styles.shopSection}>
+      <ShopHeader />
+      <ShopCarousel />
+    </section>
+  )
+}
