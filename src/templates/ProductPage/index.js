2b763062af675cd23e03da964363f7172e44766f
drillprop <drylbartosz@gmail.com>
2020-11-19 16:35:24 +0100

    Style and populate with a data ProductPage

--- src/templates/ProductPage/index.js
+++ src/templates/ProductPage/index.js
@@ -1,23 +1,41 @@
 import React from "react"
 import { graphql } from "gatsby"
+import CTAButton from "../../components/CTAButton"
+import styles from "./product-page.module.scss"
 
 export default function ProductPage({ data }) {
-  return <div>Hello product page</div>
+  const { product } = data.markdownRemark
+  const { publicURL } = data.file
+
+  return (
+    <section className={styles.productSection}>
+      <img className={styles.productImg} src={publicURL} alt={product.name} />
+      <article className={styles.productInfo}>
+        <p className={styles.productTag}>{product.tag}</p>
+        <h2 className={styles.productName}>{product.name}</h2>
+        <p className={styles.productDescription}>{product.description}</p>
+        <p className={styles.productPrice}>${product.price}</p>
+        <CTAButton>ADD TO CART</CTAButton>
+      </article>
+    </section>
+  )
 }
 
 export const query = graphql`
-  query($slug: String!) {
+  query($slug: String!, $base: String!) {
     markdownRemark(frontmatter: { slug: { eq: $slug } }) {
-      id
-      frontmatter {
+      product: frontmatter {
         name
         slug
         price
-        excerpt
         image
+        excerpt
         description
         tag
       }
+    }
+    file(base: { eq: $base }) {
+      publicURL
     }
   }
 `
