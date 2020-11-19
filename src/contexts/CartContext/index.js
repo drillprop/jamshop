b2dcfd48585cb98af49d8d406e406d456a7b354a
drillprop <drylbartosz@gmail.com>
2020-11-19 21:02:57 +0100

    Create CartContext

---
+++ src/contexts/CartContext/index.js
@@ -0,0 +1,37 @@
+import React, { createContext, useState, useContext } from "react"
+
+const CartContext = createContext(null)
+
+export default function CartContextProvider({ children }) {
+  const [isCartOpen, setIsCartOpen] = useState(false)
+  const [cartItems, setItemsInCart] = useState([])
+  const cartItemsAmount = cartItems.length
+  console.log(cartItemsAmount)
+
+  const toggleCart = () => {
+    if (!cartItemsAmount && !isCartOpen) return
+    setIsCartOpen((prevState) => !prevState)
+  }
+
+  const addItemToCart = (cartItem) => {
+    setItemsInCart((prevState) => [...prevState, cartItem])
+  }
+
+  return (
+    <CartContext.Provider
+      value={{
+        isCartOpen,
+        toggleCart,
+        cartItems,
+        cartItemsAmount,
+        addItemToCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export function useCartContext() {
+  return useContext(CartContext)
+}
