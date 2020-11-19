bc7aeaa9382bdf0393e48e028a64e3c407f68f3f
drillprop <drylbartosz@gmail.com>
2020-11-19 11:01:12 +0100

    Wrap pages in Layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 import React from "react"
-
-import Layout from "../layouts"
-import SEO from "../components/SEO"
 import Hero from "../components/Hero"
+import SEO from "../components/SEO"
 
 export default function IndexPage() {
   return (
-    <Layout>
+    <>
       <SEO title="Home" />
       <Hero />
-    </Layout>
+    </>
   )
 }
