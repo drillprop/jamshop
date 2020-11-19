03db4b27e3f74683b282d3582c591703f34c9972
drillprop <drylbartosz@gmail.com>
2020-11-19 16:36:38 +0100

    Style header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
+import styles from "./header.module.scss"
+import Carticon from "../../assets/icons/cart.svg"
 
 export default function Header() {
   return (
-    <header>
-      <div>
+    <header className={styles.header}>
+      <h1 className={styles.logo}>
         <Link to="/">JAM SHOP</Link>
-      </div>
+      </h1>
+      <button className={styles.cartButton} title="open cart">
+        <img className={styles.cartIcon} src={Carticon} alt="cart icon" />
+        <span className={styles.cartQuantity}>0</span>
+      </button>
     </header>
   )
 }
