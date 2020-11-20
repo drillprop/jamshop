3fce7fab33ab4f679e5b579827562736c185c19a
drillprop <drylbartosz@gmail.com>
2020-11-20 13:10:28 +0100

    Rename and update ShopCard

--- src/components/Shop/ShopCarousel/ShopCard/index.js
+++ src/components/Shop/ShopCarousel/ShopCard/index.js
@@ -1,8 +1,15 @@
 import React from "react"
 import AddIcon from "../../../../assets/icons/add.svg"
-import styles from "./shop-card.module.scss"
+import styles from "./ShopCard.module.scss"
+import { useCartContext } from "../../../../contexts/CartContext"
+import { Link } from "gatsby"
 
 export default function ShopCard({ product }) {
+  const { addItemToCart } = useCartContext()
+  const handleAddToCart = (e) => {
+    e.preventDefault()
+    addItemToCart(product)
+  }
   return (
     <article className={styles.cardWrapper}>
       <img
@@ -10,12 +17,18 @@ export default function ShopCard({ product }) {
         src={product.image.publicURL}
         alt={product.name}
       />
-      <section>
-        <h4 className={styles.cardTitle}>{product.name}</h4>
-        <p className={styles.cardExcerpt}>{product.excerpt}</p>
-      </section>
-      <button className={styles.cardButton} title="add product">
-        <img src={AddIcon} alt="" />
+      <Link to={`/products/${product.slug}`}>
+        <section>
+          <h4 className={styles.cardTitle}>{product.name}</h4>
+          <p className={styles.cardExcerpt}>{product.excerpt}</p>
+        </section>
+      </Link>
+      <button
+        className={styles.cardButton}
+        title="add product"
+        onClick={handleAddToCart}
+      >
+        <img src={AddIcon} alt="cart icon" />
       </button>
     </article>
   )
