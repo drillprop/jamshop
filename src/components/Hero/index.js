8ed40e8b90f1d663dd01902490167af20b3d6c8a
drillprop <drylbartosz@gmail.com>
2020-11-20 17:56:21 +0100

    Add Hero animation

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -4,16 +4,50 @@ import HeroBackground from "../../assets/elements/hero_background.svg"
 import HeroImage from "../../assets/elements/monitor.svg"
 import styles from "./Hero.module.scss"
 import CTAButton from "../CTAButton"
+import { motion } from "framer-motion"
+
+const h2variants = {
+  initial: {
+    x: 80,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.1,
+      staggerChildren: 0.15,
+      ease: [0.5, 0.05, -0.01, 0.9],
+    },
+  },
+}
+
+const spanVariants = {
+  initial: { x: 100, opacity: 0 },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.4,
+    },
+  },
+}
 
 function HomepageHero() {
   return (
     <section className={styles.heroSection}>
       <img src={HeroImage} alt="Vector Monitor" />
       <div className={styles.heroTitleWrapper}>
-        <h2 className={styles.heroTitle}>
-          <span>Don't waste time</span>
-          <span>on boring things</span>
-        </h2>
+        <motion.h2
+          initial="initial"
+          animate="animate"
+          variants={h2variants}
+          className={styles.heroTitle}
+        >
+          <motion.span variants={spanVariants}>Don't waste time</motion.span>
+          <motion.span variants={spanVariants}>on boring things</motion.span>
+          <span />
+        </motion.h2>
         <CTAButton>GO EXPLORE</CTAButton>
       </div>
       <img className={styles.heroBackground} src={HeroBackground} />
