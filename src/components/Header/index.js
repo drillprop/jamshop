82302099cbddb93239cea97aae6b6eba4b1ba364
drillprop <drylbartosz@gmail.com>
2020-11-20 17:55:35 +0100

    Add header animation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,26 @@
+import clsx from "clsx"
+import { motion } from "framer-motion"
 import { Link } from "gatsby"
 import React from "react"
-import Carticon from "../../assets/icons/cart.svg"
 import ArrowSmall from "../../assets/icons/arrow-small.svg"
+import Carticon from "../../assets/icons/cart.svg"
 import { useCartContext } from "../../contexts/CartContext"
 import CartDropdown from "./CartDropdown"
 import styles from "./Header.module.scss"
-import clsx from "clsx"
 
 export default function Header() {
   const { isCartOpen, toggleCart, cartItemsAmount } = useCartContext()
   return (
     <>
-      <header className={styles.header}>
+      <motion.header
+        animate={{ y: 0, opacity: 1 }}
+        initial={{ y: -40, opacity: 0 }}
+        transition={{
+          duration: 0.5,
+          ease: [0.5, 0.04, -0.01, 0.9],
+        }}
+        className={styles.header}
+      >
         <h1 className={styles.logo}>
           <Link to="/">JAM SHOP</Link>
         </h1>
@@ -36,7 +45,7 @@ export default function Header() {
             />
           </button>
         </div>
-      </header>
+      </motion.header>
       {isCartOpen && <CartDropdown />}
     </>
   )
