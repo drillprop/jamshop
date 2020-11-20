8ea3e3acf7b131cf48210984ad1b9db4105f7263
drillprop <drylbartosz@gmail.com>
2020-11-20 13:11:45 +0100

    Create styles for Layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 
 import Header from "../components/Header"
+import "../styles/global.scss"
+import styles from "./Layout.module.scss"
 
 export default function Layout({ children }) {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main className={styles.contentWrapper}>{children}</main>
       <footer />
     </>
   )
