9473097d31a1fd5c267b84b281c17f4206033631
drillprop <drylbartosz@gmail.com>
2020-11-20 13:11:11 +0100

    Add Slider to ShopCarousel, create style file

--- src/components/Shop/ShopCarousel/index.js
+++ src/components/Shop/ShopCarousel/index.js
@@ -1,6 +1,43 @@
-import { graphql, Link, useStaticQuery } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick-theme.css"
+import "slick-carousel/slick/slick.css"
+import ShopArrow from "./ShopArrow"
 import ShopCard from "./ShopCard"
+import styles from "./ShopCarousel.module.scss"
+
+const settings = {
+  speed: 300,
+  infinite: false,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  nextArrow: <ShopArrow direction="right" />,
+  prevArrow: <ShopArrow direction="left" />,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+}
 
 export default function ShopCarousel() {
   const data = useStaticQuery(graphql`
@@ -26,9 +63,13 @@ export default function ShopCarousel() {
   `)
   const products = data.allMarkdownRemark.nodes
 
-  return products.map(({ product }) => (
-    <Link key={product.slug} to={`/products/${product.slug}`}>
-      <ShopCard product={product} />
-    </Link>
-  ))
+  return (
+    <div className={styles.shopCarouselWrapper}>
+      <Slider {...settings}>
+        {products.map(({ product }) => (
+          <ShopCard key={product.slug} product={product} />
+        ))}
+      </Slider>
+    </div>
+  )
 }
